98fc3f7b6ff9596679ca2bad2f4e61d4af664368
Damian Barrios <125921716+DamianDrawes@users.noreply.github.com>
2023-12-02 16:44:26 -0300

    Add files via upload

--- js/boxMov.js
+++ js/boxMov.js
@@ -3,23 +3,23 @@ const boxIz=document.querySelector("#Bizq");
 const boxDr=document.querySelector("#Bder");
 function BoxmovD(){
     let productoIni=document.querySelectorAll(".boxProducto")[0];
-    BoxConteiner.style.marginLeft="-560px";
+    BoxConteiner.style.marginLeft="-440px";
     BoxConteiner.style.transition="all 0.3s";
     setTimeout(function(){
         BoxConteiner.style.transition="none";
         BoxConteiner.insertAdjacentElement("beforeend",productoIni);
-        BoxConteiner.style.marginLeft="-280px";
+        BoxConteiner.style.marginLeft="-170px";
     },300);
 }
 function BoxmovI(){
     let producto=document.querySelectorAll(".boxProducto");
     let productoFin=producto[producto.length-1];
-    BoxConteiner.style.marginLeft="0";
+    BoxConteiner.style.marginLeft="100px";
     BoxConteiner.style.transition="all 0.3s";
     setTimeout(function(){
         BoxConteiner.style.transition="none";
         BoxConteiner.insertAdjacentElement("afterbegin",productoFin);
-        BoxConteiner.style.marginLeft="-280px"; 
+        BoxConteiner.style.marginLeft="-170px"; 
     },300);
 }
 
